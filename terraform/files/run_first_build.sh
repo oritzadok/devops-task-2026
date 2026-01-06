964f7a4fe55c8a2341b087d1a8df64c6a3659f18
EC2 Default User <ec2-user@ip-172-31-19-160.ec2.internal>
2026-01-06 10:06:27 +0000

    commit 17

--- terraform/files/run_first_build.sh
+++ terraform/files/run_first_build.sh
@@ -5,13 +5,41 @@ set -euo pipefail
 ecr_repo=$1
 gh_repo=$2
 
+GH_HEADERS=(
+  -H "Accept: application/vnd.github+json"
+  -H "Authorization: Bearer ${GITHUB_TOKEN}"
+  -H "X-GitHub-Api-Version: 2022-11-28"
+)
+
 if [ "$(aws ecr list-images --repository-name $ecr_repo --query 'length(imageIds)')" = "0" ]; then
   echo "ECR repository is empty. Triggering a build"
-  curl -L \
-    -X POST \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
+  curl -L -X POST "${GH_HEADERS[@]}" \
     https://api.github.com/repos/${gh_repo}/actions/workflows/build.yaml/dispatches \
     -d '{"ref":"main"}'
+    
+    sleep 5  # Give some time to enqueue the run
+    
+    echo "Getting run ID"
+    read run_id run_url < <(curl -s "${GH_HEADERS[@]}" \
+      https://api.github.com/repos/${gh_repo}/actions/workflows/build.yaml/runs?branch=main&event=workflow_dispatch \
+      | jq -r '.workflow_runs | first | [.id, .html_url] | join(" ")')
+      
+    echo "Run URL: $run_url"
+    echo "Run ID:  $run_id"
+    
+    echo "Waiting for the run to finish"
+    while true; do
+      conclusion=$(curl -s "${GH_HEADERS[@]}" \
+        https://api.github.com/repos/${gh_repo}/actions/runs/$run_id \
+         | jq -r '.conclusion | ascii_upcase')
+
+      if [ -n "${conclusion}" ]; then
+        echo "Finished with status: ${conclusion}"
+
+        
+      fi
+    
+      echo "Workflow is in the middle of running"
+      echo "conclusion: ${conclusion}" && sleep 1 #sleep 30
+    done
 fi
