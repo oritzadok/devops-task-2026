d6fcdeb1c0b37412cd67ed6fa6c76c6345bcd29d
EC2 Default User <ec2-user@ip-172-31-19-160.ec2.internal>
2026-01-06 17:07:41 +0000

    commit 27

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello World from Ori 2!</p>"
+    return "<p>Hello World from Ori 3!</p>"
 
 @app.route("/ready")
 def ready():
